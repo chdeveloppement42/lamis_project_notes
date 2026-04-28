e5206f3069660efe2c4f3c138394532469ef280c
marrokesm <marrokmar@gmail.com>
2026-04-28 08:42:25 +0100

    clean up

--- backend/src/casl/permissions.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export type PermissionRule = { action: string; subject: string };
-export const PERMISSIONS_KEY = 'permissions_required';
-
-// Usage: @CheckPermissions({ action: 'manage', subject: 'providers' })
-export const CheckPermissions = (...requirements: PermissionRule[]) =>
-  SetMetadata(PERMISSIONS_KEY, requirements);
