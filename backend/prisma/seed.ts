700e3d43030be3e81631dbd90718df6a9fc6b7f2
abdomarrok <marrokmar@gmail.com>
2026-05-12 17:36:37 +0100

    Update database connection string in seed.ts for deployment environment

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import * as bcrypt from 'bcrypt';
 
-const pool = new Pool({ connectionString: 'postgresql://postgres:chdev2026@localhost:5432/immo_lamis?schema=public' });
+const pool = new Pool({ connectionString: 'postgresql://neondb_owner:npg_89RfaNHOCrtw@ep-polished-sky-aldxisuu-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require' });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
