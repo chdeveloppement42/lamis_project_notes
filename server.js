9db0d1adf36d2fc2fa101dba651d01a8a2de7210
abdomarrok <marrokmar@gmail.com>
2026-05-19 18:46:20 +0100

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './build/server.obfuscated.js';
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.VERCEL !== '1') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
